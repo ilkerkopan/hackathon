9285e1324d8f9ae3adf9390624cb6571bde7b38e
ilker.kopan <ilker.kopan@servicenow.com>
2023-06-02 10:01:43 +0200

    adds new isNegative method

--- gen-ai-sample-project/gen-ai-sample/src/GreetingController.java
+++ gen-ai-sample-project/gen-ai-sample/src/GreetingController.java
@@ -14,4 +14,21 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter++, template + name);
 	}
+
+    /*
+    A method that detects whether a given number is negative or not
+    */
+    private static boolean isNegative(int x, String methodname)
+    {
+    try {
+        if(x<0)
+        throw new IllegalArgumentException("Error: Non-negative integer used in method " + methodname);
+        return false;
+    }
+    catch (IllegalArgumentException e)
+    {
+        System.out.println(e);
+        return true;
+    }
+    }
 }
